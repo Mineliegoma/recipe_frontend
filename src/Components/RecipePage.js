4fb219e9e587bfeafd25f99b0c07e2f5b9ea4b8d
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-10-20 10:06:11 -0400

    x button

--- src/Components/RecipePage.js
+++ src/Components/RecipePage.js
@@ -7,7 +7,10 @@ class RecipePage extends React.Component {
         console.log(this.props.recipe)
         return (
             <div className="recipe-page" onClick={this.props.clickHandler}>
-                < h1>{this.props.recipe.title}</h1>
+                <div style={{display: "flex", justifyContent: "space-between"}}>
+                    < h1 >{this.props.recipe.title}</h1>
+                    <button type="button" className="close" font-size="25px">X</button>
+                    </div>
                 <img alt="this image no longer available" src={this.props.recipe.image} width={800}  />
                 <div dangerouslySetInnerHTML={{__html: text}} />
             </div>
